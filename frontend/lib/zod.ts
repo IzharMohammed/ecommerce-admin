92102a250e37191ae0e8e029a5eea1b6f0a74922
IzharMohammed <izharmohammed21@gmail.com>
2025-05-09 22:37:08 +0530

    Added zod validations

---
+++ frontend/lib/zod.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+
+const getNameSchema = () =>
+    z.string({ required_error: "Name is required" })
+        .min(1, "Name is required")
+        .max(50, "Name must be less than 50 characters")
+
+const getEmailSchema = () =>
+    z.string({ required_error: "Email is required" }).min(1, "Email is required").email()
+
+const getpasswordSchema = (type: "password" | "confirmPasswod") =>
+    z.string({ required_error: `${type}: is required` })
+        .min(8, `${type} must be atleast 8 characters`)
+        .max(32, `${type} can not exceed 32 characters`);
+
+const signUpSchema = z.object({
+    name: getNameSchema(),
+    email: getEmailSchema(),
+    password: getpasswordSchema("password"),
+    confirmPassword: getpasswordSchema("confirmPassword")
+})
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords don't match",
+        path: ["confirmPassword"], // path of error
+    })
+
+const signInSchema = z.object({
+    email: getEmailSchema(),
+    password: getpasswordSchema("password"),
+})
