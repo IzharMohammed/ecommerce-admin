5ea1d6a0630c825db2b9a399e9af075495496043
IzharMohammed <izharmohammed21@gmail.com>
2025-05-10 01:20:00 +0530

    Added signIn page

--- frontend/components/auth-buttons.tsx
+++ frontend/components/auth-buttons.tsx
@@ -6,23 +6,24 @@ import SignoutButton from "@/components/signout-button";
 import { Button } from "@/components/ui/button";
 
 export default function AuthButtons() {
-	const { data, isPending } = authClient.useSession();
-	if (isPending) return <div>Loading...</div>;
+  const { data, isPending } = authClient.useSession();
+  if (isPending) return <div>Loading...</div>;
 
-	const session = data;
+  const session = data;
+//   console.log(session);
 
-	return !session ? (
-		<div className="flex gap-2 justify-center">
-			<Link href="/sign-in">
-				<Button>Sign In</Button>
-			</Link>
-			<Link href="/sign-up">
-				<Button>Sign Up</Button>
-			</Link>
-		</div>
-	) : (
-		<div className="flex items-center gap-2">
-			<SignoutButton />
-		</div>
-	);
+  return !session ? (
+    <div className="flex gap-2 justify-center">
+      <Link href="/sign-in">
+        <Button>Sign In</Button>
+      </Link>
+      <Link href="/sign-up">
+        <Button>Sign Up</Button>
+      </Link>
+    </div>
+  ) : (
+    <div className="flex items-center gap-2">
+      <SignoutButton />
+    </div>
+  );
 }
