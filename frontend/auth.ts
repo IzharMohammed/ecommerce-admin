c6d5829754de596db0e9d88b394fb425216c0926
IzharMohammed <izharmohammed21@gmail.com>
2025-05-09 21:57:33 +0530

    Initialized better-auth client & email send by send grid

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,13 +1,15 @@
+import { PrismaClient } from "@prisma/client";
 import { betterAuth, BetterAuthOptions } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@prisma/client";
- 
+import { admin, openAPI } from "better-auth/plugins";
+import { sendEmail } from "./actions/email";
+
 const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
-    
+plugins:[openAPI()],   // api/auth/reference 
     emailAndPassword:{
         enabled: true,
         requireEmailVerification:true
