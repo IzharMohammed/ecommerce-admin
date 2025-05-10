4b86dfedad86f17d189aea6a95a2abaac64c2f4f
IzharMohammed <izharmohammed21@gmail.com>
2025-05-10 16:50:05 +0530

    feat:Added themes via next-themes

--- frontend/components/auth-buttons.tsx
+++ frontend/components/auth-buttons.tsx
@@ -4,16 +4,18 @@ import { authClient } from "@/auth-client";
 import Link from "next/link";
 import SignoutButton from "@/components/signout-button";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "./theme-toggle";
 
 export default function AuthButtons() {
   const { data, isPending } = authClient.useSession();
   if (isPending) return <div>Loading...</div>;
 
   const session = data;
-//   console.log(session);
+  //   console.log(session);
 
   return !session ? (
     <div className="flex gap-2 justify-center">
+      <ModeToggle />
       <Link href="/sign-in">
         <Button>Sign In</Button>
       </Link>
@@ -23,6 +25,7 @@ export default function AuthButtons() {
     </div>
   ) : (
     <div className="flex items-center gap-2">
+      <ModeToggle />
       <SignoutButton />
     </div>
   );
--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,13 +1,26 @@
 import Link from "next/link";
 import AuthButtons from "@/components/auth-buttons";
 
-export default  function Navbar() {
+export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center py-3 px-4 fixed top-0 left-0 right-0 z-50 bg-slate-100">
-      <Link href="/" className="text-xl font-bold">
-        better-auth
-      </Link>
-      <AuthButtons />
-    </nav>
+    <>
+      <nav className="flex justify-between  items-center py-3 px-4 fixed top-0 left-0 right-0 z-50 ">
+        <Link href="/" className="text-xl font-bold">
+          better-auth
+        </Link>
+        <AuthButtons />
+      </nav>
+      <div className="h-[4rem]"></div>
+    </>
   );
 }
+
+/*
+Represent data in table format with dummy data with image also in the data 
+make the dashboard mobile responsive 
+when i click on add new category or stores or deals or producs then i should see the form coming from right side 
+smoothly which contains the fields to create a new category or store or deal or product from i can add information about it 
+and also i can upload image currently by selecting from my desktop or by drag and drop(currently for image upload add it from front-end side
+i will manage uploading feature by myself) 
+
+*/
